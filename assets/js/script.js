a3cdaed7fff1118eb07d5e4c03f6b042c6c6c16c
Hua Chan <huachanzhuo@berkeley.edu>
2019-04-28 14:55:18 -0700

    added about section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,43 @@
 $(document).ready(function() {
-	// $(function() {
-	// 	$("#fullpage").fullpage({
-	// 		sectionSelector: ".page",
-	// 		scrollOverflow: true
-	// 	});
-	// });
-	$(function(){
-	  $('#fullpage').fullpage();
-	});
 
-	console.log("heeere");
+	function exposeLightbox() {
+		document.getElementById('lightbox-overlay').classList.remove('hidden');
+		var lightbox = 'moment-' + $(this).attr('id').slice(7,8) + '-description';
+		document.getElementById(lightbox).classList.remove('hidden');
+		document.getElementById(lightbox).classList.add('exposed');
+	}
+
+	function hideAllLightboxes() {
+		document.getElementById('lightbox-overlay').classList.add('hidden');
+		for (var i = 1; i <=6; i++) {
+			var lightbox = 'moment-' + i + '-description';
+			document.getElementById(lightbox).classList.remove('exposed');
+			document.getElementById(lightbox).classList.add('hidden');
+		}
+	}
+
+	function changeYear() {
+
+
+		for(var i = 2016; i <= 2018; i++) {
+			var yearItem = "cluster-" + i;
+			document.getElementById(yearItem).classList.add('hidden');
+		}
+
+		var activeYear = "cluster-" + $(this).attr('id').slice(5,10);
+		console.log(activeYear);
+		document.getElementById(activeYear).classList.remove('hidden');
+	}
+
+	document.getElementById('moment-1').onclick = exposeLightbox;
+	document.getElementById('moment-2').onclick = exposeLightbox;
+	document.getElementById('moment-3').onclick = exposeLightbox;
+	document.getElementById('moment-4').onclick = exposeLightbox;
+	document.getElementById('moment-5').onclick = exposeLightbox;
+	document.getElementById('moment-6').onclick = exposeLightbox;
+	document.getElementById('lightbox-overlay').onclick = hideAllLightboxes;
+
+	document.getElementById('year-2016').onclick = changeYear;
+	document.getElementById('year-2017').onclick = changeYear;
+	document.getElementById('year-2018').onclick = changeYear;
 });
